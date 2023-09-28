befe7774961a0c88b3e8fb7ee5f91adf6aaf3559
Daniel A. Cifuentes C. <dacifuentes@outlook.com>
2023-09-27 20:51:22 -0500

    Fix typo in regex

--- js/numberParser.js
+++ js/numberParser.js
@@ -2,19 +2,18 @@ const numberParser = () => {
 	let countryCode = document.querySelector("#country-call-code").value;
 	const number = document.querySelector("#wa-number").value;
   const waLink = document.querySelector(".wa-link");
-	const regex = /(?:\+?\d{1,3}[-\s()]?)*\d{1,3}/g;
+	const regex = /(?:\+?\d{2,3}(?:[ -]?\d{3}){2,}(?:[ -]?\d{2,})*)|(?:\+\d{2,3} \(\d{3}\) \d{2,})/g;
 	const matches = (countryCode + number).match(regex);
   let parsedNumber;
 	if (matches) {
-		for (const match of matches) {		
-			const modifiedMatch = match.startsWith("+")
-				? match.substring(1)
-				: match;
-        parsedNumber = "https://wa.me/" + modifiedMatch;
-		}
-	}
-  waLink.href = parsedNumber;
-  waLink.innerText = parsedNumber;
-  navigator.clipboard.writeText(parsedNumber);
-  alert("Copied the WhatsApp link: " + parsedNumber);
+    const modifiedMatch = matches.map(match => match.replace(/[^\d]/g, '')).join('');
+    parsedNumber = "https://wa.me/" + modifiedMatch;
+    waLink.href = parsedNumber;
+    waLink.innerText = parsedNumber;
+    navigator.clipboard.writeText(parsedNumber);
+    alert("Copied the WhatsApp link: " + parsedNumber);
+	} else {
+    alert("Input field must contain a valid phone number");
+  }
+
 };
